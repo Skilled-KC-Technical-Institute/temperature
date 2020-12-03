071e5473b5d16c3b6884b7d1fac2a5304e5604eb
jhatfieldkc <jhatfieldkc@gmail.com>
2020-12-02 22:57:48 -0600

    added bootstrap
    created bootstrap grid. added css file

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function fromFahrenheit() {
     document.getElementById("kelValue").value = Math.round(((document.getElementById("farValue").value - 32) / 1.8 + 273.15) * 100) / 100;
 }
 function fromCelsius() {
-    document.getElementById("farValue").value = Math.round(document.getElementById("celValue").value * 1.8 + 32 * 100) / 100;
-    document.getElementById("kelValue").value = Math.round((document.getElementById("celValue").value * 1) + 273.15 * 100) / 100;
+    document.getElementById("farValue").value = Math.round((document.getElementById("celValue").value * 1.8 + 32) * 100) / 100;
+    document.getElementById("kelValue").value = Math.round(((document.getElementById("celValue").value * 1) + 273.15) * 100) / 100;
 }
 function fromKelvin() {
     document.getElementById("farValue").value = Math.round(((document.getElementById("kelValue").value - 273.15) * 1.8 + 32) * 100) / 100;
