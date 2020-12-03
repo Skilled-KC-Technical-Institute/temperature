b3e763648bf94b6534d7ec02b0ca1fe793a9c863
jhatfieldkc <jhatfieldkc@gmail.com>
2020-12-03 15:51:51 -0600

    final draft
    final draft

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 function fromFahrenheit() {
-    document.getElementById("celValue").value = Math.round((document.getElementById("farValue").value - 32) / 1.8 * 100) / 100;
-    document.getElementById("kelValue").value = Math.round(((document.getElementById("farValue").value - 32) / 1.8 + 273.15) * 100) / 100;
+    if (document.getElementById("farValue").value.length == 0) {
+        document.getElementById("celValue").value = ""
+        document.getElementById("kelValue").value = ""
+    }
+    else {
+        document.getElementById("celValue").value = Math.round((document.getElementById("farValue").value - 32) / 1.8 * 100) / 100;
+        document.getElementById("kelValue").value = Math.round(((document.getElementById("farValue").value - 32) / 1.8 + 273.15) * 100) / 100;    
+    }
 }
 function fromCelsius() {
-    document.getElementById("farValue").value = Math.round((document.getElementById("celValue").value * 1.8 + 32) * 100) / 100;
-    document.getElementById("kelValue").value = Math.round(((document.getElementById("celValue").value * 1) + 273.15) * 100) / 100;
+    if (document.getElementById("celValue").value.length == 0) {
+        document.getElementById("farValue").value = ""
+        document.getElementById("kelValue").value = ""
+    }
+    else {
+        document.getElementById("farValue").value = Math.round((document.getElementById("celValue").value * 1.8 + 32) * 100) / 100;
+        document.getElementById("kelValue").value = Math.round(((document.getElementById("celValue").value * 1) + 273.15) * 100) / 100;
+    }
 }
 function fromKelvin() {
-    document.getElementById("farValue").value = Math.round(((document.getElementById("kelValue").value - 273.15) * 1.8 + 32) * 100) / 100;
-    document.getElementById("celValue").value = Math.round(document.getElementById("kelValue").value - 273.15 * 100) / 100;
+    if (document.getElementById("kelValue").value.length == 0) {
+        document.getElementById("farValue").value = ""
+        document.getElementById("celValue").value = ""
+    }
+    else {
+        document.getElementById("farValue").value = Math.round(((document.getElementById("kelValue").value - 273.15) * 1.8 + 32) * 100) / 100;
+        document.getElementById("celValue").value = Math.round((document.getElementById("kelValue").value - 273.15) * 100) / 100;    
+    }
 }
